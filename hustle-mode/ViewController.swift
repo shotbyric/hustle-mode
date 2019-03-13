9a1c3ef46148df62754d43ee670a6b63020fdf82
Ricardo Castillo <ricardocastillo@ricardos-macbook-pro.local>
2019-03-12 22:31:13 -0700

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Ricardo Castillo on 2019-03-12.
+//  Copyright © 2019 Ricardo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
